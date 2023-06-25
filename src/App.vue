f108d9e30c3defdad4b7042191d3fdd2b134919b
Yufei Ma <moana.yf@gmail.com>
2023-06-26 02:50:07 +0800

    starting from scratch

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <div class="flex">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
