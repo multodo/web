0a00a2bd20e8cf768e2ff33569f9fb751f2ad7a4
Yufei Ma <moana.yf@gmail.com>
2023-06-27 17:13:12 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
 </script>
 
 <template>
-  <main class="bg-black h-screen"></main>
+  <main class="h-screen flex flex-row">
+
+    <section class="w-1/2 bg-blue-700 h-screen">
+
+    </section>
+    <section class="w-1/2 bg-yellow-300 h-screen"></section>
+    
+  </main>
+  <div class="relative bottom-12 text-center">floating</div>
 </template>
 
